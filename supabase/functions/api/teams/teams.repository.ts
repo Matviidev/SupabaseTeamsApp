6e5d11fd75693d1af06b25af59fda26e95a40987
Maviidev <matvii.shpyruk.dev@gmail.com>
2026-01-30 14:36:22 +0200

    feat: add team repository layer

---
+++ supabase/functions/api/teams/teams.repository.ts
@@ -0,0 +1,51 @@
+import { Expression, ExpressionBuilder, Insertable, Kysely } from "kysely";
+import { jsonArrayFrom } from "kysely/helpers/postgres";
+import { DB, Teams } from "../db/types.ts";
+
+interface SelectOptions {
+  omitUsers?: boolean;
+}
+
+function users(eb: ExpressionBuilder<DB, "teams">, teamId: Expression<string>) {
+  return jsonArrayFrom(
+    eb
+      .selectFrom("profiles")
+      .select(["id", "fullName", "avatarUrl", "isOnline", "lastSeen"])
+      .whereRef("profiles.teamId", "=", teamId),
+  ).as("users");
+}
+
+export const create = (db: Kysely<DB>, data: Insertable<Teams>) => {
+  return db
+    .insertInto("teams")
+    .values(data)
+    .returningAll()
+    .executeTakeFirstOrThrow();
+};
+
+export const findByInviteCode = (db: Kysely<DB>, code: string) => {
+  return db
+    .selectFrom("teams")
+    .where("inviteCode", "=", code)
+    .selectAll()
+    .executeTakeFirst();
+};
+
+export const findById = (db: Kysely<DB>, id: string, opt?: SelectOptions) => {
+  return db
+    .selectFrom("teams")
+    .where("id", "=", id)
+    .selectAll()
+    .$if(!opt?.omitUsers, (qb) =>
+      qb.select((eb) => [users(eb, eb.ref("teams.id"))]),
+    )
+    .executeTakeFirst();
+};
+
+export const findTeamUsers = (db: Kysely<DB>, id: string) => {
+  return db
+    .selectFrom("profiles")
+    .where("teamId", "=", id)
+    .selectAll()
+    .execute();
+};
