1b1a5665231da2c87b38aae5318bb20285674cf6
Maviidev <matvii.shpyruk.dev@gmail.com>
2026-02-01 15:00:57 +0200

    feat: add initial database migration

---
+++ supabase/migrations/20260129163310_initial_migation.sql
@@ -0,0 +1,73 @@
+
+  create table "public"."profiles" (
+    "id" uuid not null default gen_random_uuid(),
+    "created_at" timestamp with time zone not null default now(),
+    "first_name" text not null,
+    "last_name" text not null,
+    "avatar_url" text
+      );
+
+
+alter table "public"."profiles" enable row level security;
+
+CREATE UNIQUE INDEX profiles_pkey ON public.profiles USING btree (id);
+
+alter table "public"."profiles" add constraint "profiles_pkey" PRIMARY KEY using index "profiles_pkey";
+
+grant delete on table "public"."profiles" to "anon";
+
+grant insert on table "public"."profiles" to "anon";
+
+grant references on table "public"."profiles" to "anon";
+
+grant select on table "public"."profiles" to "anon";
+
+grant trigger on table "public"."profiles" to "anon";
+
+grant truncate on table "public"."profiles" to "anon";
+
+grant update on table "public"."profiles" to "anon";
+
+grant delete on table "public"."profiles" to "authenticated";
+
+grant insert on table "public"."profiles" to "authenticated";
+
+grant references on table "public"."profiles" to "authenticated";
+
+grant select on table "public"."profiles" to "authenticated";
+
+grant trigger on table "public"."profiles" to "authenticated";
+
+grant truncate on table "public"."profiles" to "authenticated";
+
+grant update on table "public"."profiles" to "authenticated";
+
+grant delete on table "public"."profiles" to "postgres";
+
+grant insert on table "public"."profiles" to "postgres";
+
+grant references on table "public"."profiles" to "postgres";
+
+grant select on table "public"."profiles" to "postgres";
+
+grant trigger on table "public"."profiles" to "postgres";
+
+grant truncate on table "public"."profiles" to "postgres";
+
+grant update on table "public"."profiles" to "postgres";
+
+grant delete on table "public"."profiles" to "service_role";
+
+grant insert on table "public"."profiles" to "service_role";
+
+grant references on table "public"."profiles" to "service_role";
+
+grant select on table "public"."profiles" to "service_role";
+
+grant trigger on table "public"."profiles" to "service_role";
+
+grant truncate on table "public"."profiles" to "service_role";
+
+grant update on table "public"."profiles" to "service_role";
+
+
